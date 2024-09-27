8376208edc427678cfd06bf22f14c789a5e2fd5a
muhammad-salman-saleem <msalman1221998@gmail.com>
2024-09-27 23:49:22 +0500

    update code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN , 
+    // origin: process.env.CORS_ORIGIN , 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   })
